370fc966d626a391953faa096d64ed7c56cae698
Talley Lambert <talley.lambert@gmail.com>
2019-03-04 17:04:29 -0500

    fix otf tests

--- test/test_otf.py
+++ test/test_otf.py
@@ -2,41 +2,55 @@ import unittest
 import os
 import numpy as np
 from pycudadecon.otf import makeotf
-from pycudadecon.util import imread
+from pycudadecon.util import imread, is_otf
 
 
 class TestMakeOTF(unittest.TestCase):
+    def setUp(self):
+        self.psf = os.path.join(os.path.dirname(__file__), 'test_data', 'psf.tif')
+        self.otf = os.path.join(os.path.dirname(__file__), 'test_data', 'otf.tif')
+        self.dest = os.path.join(os.path.dirname(__file__), 'test_data', 'otf_test.tif')
+
     def test_make_otf(self):
         """
         Test that it can make an otf file from a psf
         """
-        psf = os.path.join(os.path.dirname(__file__), 'test_data', 'psf.tif')
-        self.result = makeotf(psf)
-        self.assertTrue(os.path.isfile(self.result))
+        result = makeotf(self.psf, self.dest)
+        self.assertTrue(os.path.isfile(result))
+        known = imread(self.otf)
+
+        # make sure it matches the known OTF
+        self.assertTrue(np.allclose(imread(result), known))
+        os.remove(self.dest)
 
     def test_make_otf_auto(self):
         """
         Test that it can make an otf file from a psf
         """
-        psf = os.path.join(os.path.dirname(__file__), 'test_data', 'psf.tif')
-        self.result = makeotf(psf, otf_bgrd='auto')
-        self.assertTrue(os.path.isfile(self.result))
+        result = makeotf(self.psf, self.dest, otf_bgrd='auto')
+        self.assertTrue(os.path.isfile(result))
+        os.remove(self.dest)
 
-    def text_check_otf(self):
+    def test_is_otf(self):
         """
-        Test that the new otf looks about right
+        Test that it can make an otf file from a psf
         """
-        otf = os.path.join(os.path.dirname(__file__), 'test_data', 'otf.tif')
-        newotf = imread(self.result)
-        known = imread(otf)
-        self.assertTrue(np.allclose(newotf, known))
+        self.assertTrue(is_otf(self.otf))
+
+    def test_is_not_otf(self):
+        """
+        Test that it can make an otf file from a psf
+        """
+        self.assertFalse(is_otf(self.psf))
 
     def tearDown(self):
         """
         clean up the temporary OTF file we made
         """
-        os.remove(os.path.join(os.path.dirname(__file__), 'test_data', 'psf_otf.tif'))
-
+        try:
+            os.remove(self.dest)
+        except Exception:
+            pass
 
 if __name__ == '__main__':
     unittest.main()
