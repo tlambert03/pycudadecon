95184da615db38931facd90b3f205cd3c960c976
Talley Lambert <talley.lambert@gmail.com>
2019-03-04 20:54:51 -0500

    stub out stdout_redirector [skip ci]

--- pycudadecon/util.py
+++ pycudadecon/util.py
@@ -4,6 +4,7 @@ import ctypes
 import tifffile as tf
 import numpy as np
 import warnings
+from contextlib import contextmanager
 
 
 PLAT = sys.platform
@@ -30,7 +31,9 @@ def array_is_otf(arr):
         return False
     if arr.shape[0] > arr.shape[1]:
         return False
-    if not arr[:, 1].any():
+
+    # the first pixel of an OTF will always be 1.0 and the second column 0s
+    if arr[0, 0] == 1 and (not arr[:, 1].any()):
         return True
 
 
@@ -123,3 +126,34 @@ def load_lib(libname):
             raise Exception('didn\'t find it')
         except Exception:
             continue
+
+
+# https://stackoverflow.com/questions/5081657/how-do-i-prevent-a-c-shared-library-to-print-on-stdout-in-python/17954769#17954769
+@contextmanager
+def stdout_redirected(to=os.devnull):
+    '''
+    import os
+
+    with stdout_redirected(to=filename):
+        print("from Python")
+        os.system("echo non-Python applications are also supported")
+    '''
+    fd = sys.stdout.fileno()
+
+    # assert that Python and C stdio write using the same file descriptor
+    # assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
+
+    def _redirect_stdout(to):
+        sys.stdout.close() # + implicit flush()
+        os.dup2(to.fileno(), fd) # fd writes to 'to' file
+        sys.stdout = os.fdopen(fd, 'w') # Python writes to fd
+
+    with os.fdopen(os.dup(fd), 'w') as old_stdout:
+        with open(to, 'w') as file:
+            _redirect_stdout(to=file)
+        try:
+            yield # allow code to be run with the redirected stdout
+        finally:
+            _redirect_stdout(to=old_stdout) # restore stdout.
+                                            # buffering and flags such as
+                                            # CLOEXEC may be different
