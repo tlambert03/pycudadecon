2a1de383c171fa5d34d4dd5a8559345ab3b31011
Talley Lambert <talley.lambert@gmail.com>
2019-03-04 22:04:00 -0500

    [skip ci] autodoc_mock_imports

--- docs/conf.py
+++ docs/conf.py
@@ -14,19 +14,9 @@
 #
 import os
 import sys
-from unittest.mock import MagicMock
 
 sys.path.insert(0, os.path.abspath('..'))
-
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-
-MOCK_MODULES = ['numpy', 'tifffile']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+autodoc_mock_imports = ['numpy', 'tifffile']
 
 
 # -- Project information -----------------------------------------------------
